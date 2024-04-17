1e132c1f3139da8f34582e37abad9a08553b33ea
DebarshiChakrabortyDevopsExpert <166385395+DebarshiChakrabortyDevopsExpert@users.noreply.github.com>
2024-04-18 00:17:45 +0530

    Update main.py

--- realtimespeechtotext/main.py
+++ realtimespeechtotext/main.py
@@ -8,34 +8,7 @@ from aiohttp_wsgi import WSGIHandler
 from transcript import generate_transcript
 from typing import Dict, Callable
 
-
-#load_dotenv()
-
 app = Flask('aioflask')
-
-#dg_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
-
-# async def process_audio(fast_socket: web.WebSocketResponse):
-#     async def get_transcript(data: Dict) -> None:
-#         if 'channel' in data:
-#             transcript = data['channel']['alternatives'][0]['transcript']
-        
-#             if transcript:
-#                 await fast_socket.send_str(transcript)
-
-#     deepgram_socket = await generate_transcript(web.WebSocketResponse)
-
-#     return deepgram_socket
-
-# async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]) -> str:
-#     try:
-#         socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
-#         socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
-#         socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
-
-#         return socket
-#     except Exception as e:
-#         raise Exception(f'Could not open socket: {e}')
 
 @app.route('/')
 def index():
